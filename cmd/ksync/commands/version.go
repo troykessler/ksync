92baa61533fc4915663908c630c402bb0c629479
Troy Kessler <troy.kessler99@gmail.com>
2023-09-28 11:26:40 +0200

    chore: version

--- cmd/ksync/commands/version.go
+++ cmd/ksync/commands/version.go
@@ -7,23 +7,31 @@ import (
 	"strings"
 )
 
+var version string
+
 func init() {
 	rootCmd.AddCommand(versionCmd)
 }
 
-func getVersion() string {
-	version, ok := debug.ReadBuildInfo()
+func printVersion() {
+	buildInfo, ok := debug.ReadBuildInfo()
 	if !ok {
 		panic("failed to get ksync version")
 	}
 
-	return strings.TrimSpace(version.Main.Version)
+	// only use version from build info if no version was given with
+	// -ldflags "-X 'github.com/KYVENetwork/ksync/cmd/ksync/commands.version=v1.2.3'"
+	if version == "" {
+		version = strings.TrimSpace(buildInfo.Main.Version)
+	}
+
+	fmt.Println(version)
 }
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version of KSYNC",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(getVersion())
+		printVersion()
 	},
 }
